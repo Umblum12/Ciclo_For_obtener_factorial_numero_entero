f903ad9448e767cf88b45a4d7660ada22b5613c6
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:23:15 -0600

    Agregar archivos de proyecto.

---
+++ Ciclo_For_obtener_factorial_numero_entero/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n, cont, fact;
+Console.WriteLine("Digite el valor de n: ");
+n = Convert.ToInt32(Console.ReadLine());
+if(n == 0)
+{
+    Console.WriteLine("El factorial de {0} es 1", n);
+}
+else
+{
+    fact = n;
+    for (cont = n; cont > 1; cont--)
+    {
+        fact = fact * (cont - 1);
+    }
+    Console.WriteLine("El factorial de {0} es {1}", n, fact);
+}
+Console.ReadKey(true);
